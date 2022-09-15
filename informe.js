5bddee94c1c4762cda7ee90426da21a8e0c03d2c
Cristian <cristianponce.unaj@gmail.com>
2022-09-15 10:37:53 -0300

    edit

--- informe.js
+++ informe.js
@@ -2,12 +2,18 @@
 import {
     getTask,getTasks,getX
   } from "./firebase.js";
-  
+
+
+  // UNIDAD DE BOMBEO
   const designacionApi = document.getElementById("designacion-api");
   const sentidoRotacion = document.getElementById("sentido-rotacion");
   const carreraApi = document.getElementById("carrera-api");
   const gpmApi = document.getElementById("gpm-api");
 
+  // MOTOR
+  const tipoMotor = document.getElementById("tipo-motor");
+  
+  // DATOS DE PRODUCCION
   const porcentajeAgua = document.getElementById("porcentaje-agua");
   //const designacionApi = document.getElementById("designacion-api");
   //const tasksContainer = document.getElementById("tasks-container"); 
@@ -27,11 +33,16 @@ import {
     
     console.log(ConfigDoc)
     console.log(Config)
+
+    // UNIDAD DE BOMBEO
     designacionApi.innerHTML = Config.UnidadBombeo.DesignacionApi;
     sentidoRotacion.innerHTML = Config.UnidadBombeo.SentidoRotacion;
     carreraApi.innerHTML = Config.UnidadBombeo.CarreraApi;
     gpmApi.innerHTML = Config.UnidadBombeo.GpmApi;
-    
+    // MOTOR
+    tipoMotor.innerHTML = Config.Motor.Tipo;
+
+    // DATOS DE PRODUCCION
     porcentajeAgua.innerHTML = Config.DatosProduccion.PorcentajeAgua;
     
     });
