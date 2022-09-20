faba915832f0ee53838813add2018aa6939b202f
Cristian <71187504+Cristian-E-Ponce@users.noreply.github.com>
2022-09-20 03:00:13 -0300

    edit-config

--- firebase.js
+++ firebase.js
@@ -17,13 +17,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBbPDr5Nni9jZShTbeGOwdPP4tDTvCOT3w",
-    authDomain: "dina-723d7.firebaseapp.com",
-    projectId: "dina-723d7",
-    storageBucket: "dina-723d7.appspot.com",
-    messagingSenderId: "422826683916",
-    appId: "1:422826683916:web:86483c9446f14013b70829",
-    measurementId: "G-535XCQ6R7Y"
+  apiKey: "AIzaSyC7-z6IgK5taA1znunC9WRWJeTMxu7K07o",
+  authDomain: "fir-crud-33132.firebaseapp.com",
+  projectId: "fir-crud-33132",
+  storageBucket: "fir-crud-33132.appspot.com",
+  messagingSenderId: "845976642078",
+  appId: "1:845976642078:web:e8f99cd887dc4b1661840f"
 };
 
 // Initialize Firebase
--- python/upload_dinas_data.py
+++ python/upload_dinas_data.py
@@ -19,9 +19,9 @@ from firebase_admin import firestore
 #from firebase_admin import auth
 from firebase_admin import db
 
-cred = credentials.Certificate("python/dina-723d7-firebase-adminsdk-k2ugx-8933232ee8.json")
+cred = credentials.Certificate("python/fir-crud-33132-firebase-adminsdk-eqg09-ba5c1529d4")
 app = firebase_admin.initialize_app(cred, {
-    'databaseURL' : 'https://dina-723d7.firebaseio.com'
+    'databaseURL' : 'https://fir-crud-33132.firebaseio.com'
 })
 #print(app.name) 
 db = firestore.client()
@@ -30,11 +30,10 @@ query = db.collection('Dinamometrias')
 #query.get()
 
 
-# for eco in query.stream():
-#     print('ID : {} --> {} '.format(eco.id , eco.to_dict()))
+for eco in query.stream():
+    print('ID : {} --> {} '.format(eco.id , eco.to_dict()))
 
 
-query.push({'tipo':'NoteBook','marca':'Acer','modelo':'one'})
 
 
 
