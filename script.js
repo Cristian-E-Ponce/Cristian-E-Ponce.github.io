dce4d19fb5f90fa852533ee6d6c7601b4d90f012
Cristian <71187504+Cristian-E-Ponce@users.noreply.github.com>
2023-09-09 15:08:18 -0300

    edit

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
-var sortable = $('.sortable');
-sortable.sortable({
-  axis: 'y',
-  cursor: 'move',
-  cursorAt: {top: 10},
-  handle: '.toggle',
-  // helper: 'clone',
-  opacity: '0.5',
-  // placeholder: 'sortable-placeholder',
-  change: function(event, ui) {
-    var item = $('li.ui-sortable-placeholder', sortable),
-        num;
-    // console.log('change');
-    // console.log(ui);
-    // console.log($(this));
-    // console.log($('.num', item).text());
-    $('li:not(.ui-sortable-helper)', sortable).each(function(index, item) {
-      // console.log(item);
-      // console.log($('.num', item));
-      // $('.num', item).text(index + 1);
-      if ($('.num', item).length != 0) {
-        // console.log(item);
-        $('.num', item).text(index + 1);
-      } else {
-        console.log(index +1);
-        $(".ui-sortable-helper .num").text(index + 1);
-      }
-    });
-    // $(".num", ui.item).text($('.num', item).text());
-  }
-})
-.disableSelection();
+// var sortable = $('.sortable');
+// sortable.sortable({
+//   axis: 'y',
+//   cursor: 'move',
+//   cursorAt: {top: 10},
+//   handle: '.toggle',
+//   // helper: 'clone',
+//   opacity: '0.5',
+//   // placeholder: 'sortable-placeholder',
+//   change: function(event, ui) {
+//     var item = $('li.ui-sortable-placeholder', sortable),
+//         num;
+//     // console.log('change');
+//     // console.log(ui);
+//     // console.log($(this));
+//     // console.log($('.num', item).text());
+//     $('li:not(.ui-sortable-helper)', sortable).each(function(index, item) {
+//       // console.log(item);
+//       // console.log($('.num', item));
+//       // $('.num', item).text(index + 1);
+//       if ($('.num', item).length != 0) {
+//         // console.log(item);
+//         $('.num', item).text(index + 1);
+//       } else {
+//         console.log(index +1);
+//         $(".ui-sortable-helper .num").text(index + 1);
+//       }
+//     });
+//     // $(".num", ui.item).text($('.num', item).text());
+//   }
+// })
+// .disableSelection();
 
-$('input', sortable).bind('mousedown', function(evt) {
-  evt.stopPropagation();
+// $('input', sortable).bind('mousedown', function(evt) {
+//   evt.stopPropagation();
+// });
+// $('.remove', sortable).bind({
+//   mousedown: function(evt) {
+//     evt.stopPropagation();
+//   },
+//   click: function(evt) {
+//     var currentRow = $(this).parents('li'),
+//         nextAllRow = currentRow.nextAll();
+//     nextAllRow.each(function(index, item) {
+//       $('.num', item).text($('.num', item).text() - 1);
+//     });
+//     currentRow.remove();
+//   }
+// });
+// // console.log(window.location);
+
+
+$(document).ready(function () {
+  // Hacer que los elementos de la lista sean reordenables
+  $(".sortable").sortable();
+
+  // Cambiar los valores de las contraseñas
+  $(".sortable").on("change", "input[type='text']", function () {
+    // Aquí puedes agregar la lógica para cambiar el valor de la contraseña
+    // Por ejemplo, puedes usar $(this).val() para obtener el nuevo valor del input.
+  });
 });
-$('.remove', sortable).bind({
-  mousedown: function(evt) {
-    evt.stopPropagation();
-  },
-  click: function(evt) {
-    var currentRow = $(this).parents('li'),
-        nextAllRow = currentRow.nextAll();
-    nextAllRow.each(function(index, item) {
-      $('.num', item).text($('.num', item).text() - 1);
-    });
-    currentRow.remove();
-  }
-});
-// console.log(window.location);
